28823de4c236269fff85d1daef6b312edd4203cd
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2022-02-25 16:20:48 +0700

    Fixing Base URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,6 @@
 export const API = () => {
   const baseUrl =
-    process.env.SERVER_URL ||
-    "https://be-b29-dumbmerch.herokuapp.com/api/v1" ||
-    "https://localhost:5000/api/v1";
+    process.env.BASE_URL || "https://be-b29-dumbmerch.herokuapp.com/api/v1";
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
